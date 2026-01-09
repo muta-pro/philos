35d6c37d5f0e7a956b2ca4600dfd184dc2760dbf
Ivan Mutavdzic <imutavdz@f1r7s1.codam.nl>
2026-01-09 17:09:51 +0100

    head

--- head.h
+++ head.h
@@ -6,7 +6,7 @@
 /*   By: imutavdz <imutavdz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/04 20:50:12 by imutavdz          #+#    #+#             */
-/*   Updated: 2026/01/04 22:46:59 by imutavdz         ###   ########.fr       */
+/*   Updated: 2026/01/09 16:24:29 by imutavdz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,36 +15,42 @@
 
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <sys/time.h>
+
+typedef struct info t_info;
 
 typedef struct ph
 {
-	int id;
-	int died;
-	int ate;
-	int life;
+	char			*name;
+	int				*id;
+	int				died;
+	int				times_ate;
+	long			last_time_ate;
+	pthread_t		thread_id;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
+	pthread_mutex_t	*print_lock;
+	pthread_mutex_t	*plate_lock;
+	t_info			*data;
+
 }	t_ph;
 
 
-typedef struct die
+
+typedef struct info
 {
+	int				num_of_philos;
+	int				count_must_eat;
+	int				sim_run;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	write_lock;
+	pthread_mutex_t	dead_lock;
+	pthread_mutex_t	plate_lock;
+	t_ph			*philos;
+}	t_info;
 
-}	t_die;
-
-typedef struct eat
-{
-
-}	t_eat;
-
-typedef struct sleep
-{
-
-}	t_sleep;
-
-typedef struct time
-{
-	t_die	death;
-	t_eat	eat;
-	t_sleep snore;
-}	t_time;
+int	ft_atoi(const char *str);
 
 #endif
